d42518db414255cc90a14944de905481eba7b1ec
Felipe Mayer <mayer.felipe@outlook.com>
2024-03-05 00:04:25 -0300

    feat(overview): turn card into button and add style

--- frontend/src/components/Cards/StatsCard.vue
+++ frontend/src/components/Cards/StatsCard.vue
@@ -1,18 +1,13 @@
 <template>
-  <card class="card-stats">
+  <button class="card card-stats card-amazing" @click="$emit('click')">
     <div class="row">
-      <div class="col-5" v-if="$slots.header">
-        <div class="icon-big text-center">
-          <slot name="header"></slot>
-        </div>
-      </div>
-      <div class="col-7" v-if="$slots.content">
+      <div class="col text-center">
         <div class="numbers">
           <slot name="content"></slot>
         </div>
       </div>
     </div>
-  </card>
+  </button>
 </template>
 <script>
   import Card from './Card.vue'
@@ -20,10 +15,42 @@
     name: 'stats-card',
     components: {
       Card
-    }
+    },
+    emits: ['click']
+  }
+</script>
+<style scoped>
+  button.card-amazing {
+    box-shadow: 0 0 20px rgba(0,0,0,0.1);
   }
 
-</script>
-<style>
+  button.card.card-amazing {
+    background-color: var(--white);
+    border: 1px solid var(--light-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    padding: 1.25rem;
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1) ease;
+    width: 100%;
+  }
 
+  button.card.card-amazing:hover,
+  button.card.card-amazing:active {
+    transform: scale(1.05);
+    transition-duration: 0.1s;
+  }
+
+  button.card.card-amazing:active {
+    transition-duration: 0s;
+  }
+
+  .numbers {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
 </style>
+
+
--- frontend/src/pages/Overview.vue
+++ frontend/src/pages/Overview.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-xl-2">
-          <stats-card>
+          <stats-card @click="openInNewTab('http://casadofocky.ddns.net:8989')">
             <div slot="header" class="icon-warning">
               <i class="nc-icon nc-chart text-warning"></i>
             </div>
@@ -14,7 +14,7 @@
         </div>
 
         <div class="col-xl-2">
-          <stats-card>
+          <stats-card @click="openInNewTab('http://casadofocky.ddns.net:7878')">
             <div slot="header" class="icon-success">
               <i class="nc-icon nc-light-3 text-success"></i>
             </div>
@@ -25,7 +25,7 @@
         </div>
 
         <div class="col-xl-2">
-          <stats-card>
+          <stats-card @click="openInNewTab('http://casadofocky.ddns.net:8096')"><
             <div slot="header" class="icon-danger">
               <i class="nc-icon nc-vector text-danger"></i>
             </div>
@@ -36,7 +36,7 @@
         </div>
 
         <div class="col-xl-2">
-          <stats-card>
+          <stats-card @click="openInNewTab('http://casadofocky.ddns.net:8989')">
             <div slot="header" class="icon-info">
               <i class="nc-icon nc-favourite-28 text-primary"></i>
             </div>
@@ -84,92 +84,11 @@
       ChartCard,
       StatsCard
     },
-    data () {
-      return {
-        editTooltip: 'Edit Task',
-        deleteTooltip: 'Remove',
-        pieChart: {
-          data: {
-            labels: ['40%', '20%', '40%'],
-            series: [40, 20, 40]
-          }
-        },
-        lineChart: {
-          data: {
-            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-            series: [
-              [287, 385, 490, 492, 554, 586, 698, 695],
-              [67, 152, 143, 240, 287, 335, 435, 437],
-              [23, 113, 67, 108, 190, 239, 307, 308]
-            ]
-          },
-          options: {
-            low: 0,
-            high: 800,
-            showArea: false,
-            height: '245px',
-            axisX: {
-              showGrid: false
-            },
-            lineSmooth: true,
-            showLine: true,
-            showPoint: true,
-            fullWidth: true,
-            chartPadding: {
-              right: 50
-            }
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        barChart: {
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-              [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-            ]
-          },
-          options: {
-            seriesBarDistance: 10,
-            axisX: {
-              showGrid: false
-            },
-            height: '245px'
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
-          ]
-        },
-        tableData: {
-          data: [
-            {title: 'Sign contract for "What are conference organizers afraid of?"', checked: false},
-            {title: 'Lines From Great Russian Literature? Or E-mails From My Boss?', checked: true},
-            {
-              title: 'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
-              checked: true
-            },
-            {title: 'Create 4 Invisible User Experiences you Never Knew About', checked: false},
-            {title: 'Read "Following makes Medium better"', checked: false},
-            {title: 'Unfollow 5 enemies from twitter', checked: false}
-          ]
-        }
+    methods: {
+      openInNewTab(url) {
+        window.open(url, '_blank');
       }
-    }
+    },
   }
 </script>
 <style>
